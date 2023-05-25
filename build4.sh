15217eb54b3085573a3c58903c6e210b4a5f9edc
toxsoft build server <kovach@toxsoft.ru>
2023-05-25 16:43:43 +0300

    autobuild: ts4-skide, Чт 25 мая 2023 16:43:01 MSK.

--- build4.sh
+++ build4.sh
@@ -127,7 +127,7 @@ buildTarget () {
      # set errored flag
      # echo ${BUILT_DATE} > "/tmp/${TS4_REPO}-${ERRORED_SUFFIX}"
      # set mail
-     mail -s "${TS4_GIT_SUBJECT_ERROR}${TS4_REPO}" ${TS4_MAIL_ADMINS} <<< "${TS4_GIT_FETCH_MESSAGE_ERROR}${TS4_REPO}"
+     ### mail -s "${TS4_GIT_SUBJECT_ERROR}${TS4_REPO}" ${TS4_MAIL_ADMINS} <<< "${TS4_GIT_FETCH_MESSAGE_ERROR}${TS4_REPO}"
      popd
      return 4
   fi  

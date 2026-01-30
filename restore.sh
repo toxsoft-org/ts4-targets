f7fef7e532dec07ec0bdd2170a601a93617b98ae
tsdev <tsdev@nm-dch2>
2026-01-30 18:41:05 +0700

    ts4-targets, restore.sh: +skt-sitrol/targets

--- restore.sh
+++ restore.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
-# Сохраняем текущий каталог
-current_dir=$(pwd)
+# Функция для обработки каталога
+process_directory() {
+    local target_dir="$1"
+    echo "target_dir = ${target_dir}"
+    # Ищем все подкаталоги lib, main, rcp, rap в текущем каталоге и его подкаталогах
+    find "$target_dir" -type d \( -name "lib" -o -name "plugins" -o -name "deploy" \) | while read -r target_dir; do
 
-# Ищем все подкаталоги lib, main, rcp, rap в текущем каталоге и его подкаталогах
-find "$current_dir" -type d \( -name "lib" -o -name "plugins" \) | while read -r target_dir; do
+        # Проверяем, есть ли jar-файлы в каталоге
+        if find "$target_dir" -maxdepth 1 -name "*.jar" | grep -q .; then
+            echo "Удаление jar-файлов из: $target_dir"
 
-    # Проверяем, есть ли jar-файлы в каталоге
-    if find "$target_dir" -maxdepth 1 -name "*.jar" | grep -q .; then
-        echo "Удаление jar-файлов из: $target_dir"
+            # Удаляем все jar-файлы в текущем целевом каталоге
+            find "$target_dir" -maxdepth 1 -name "*.jar" -delete
 
-        # Удаляем все jar-файлы в текущем целевом каталоге
-        find "$target_dir" -maxdepth 1 -name "*.jar" -delete
+            # Возвращаемся в родительский каталог целевого
+            parent_dir=$(dirname "$target_dir")
+            cd "$parent_dir" || exit 1
 
-        # Возвращаемся в родительский каталог целевого
-        parent_dir=$(dirname "$target_dir")
-        cd "$parent_dir" || exit 1
+            # Восстанавливаем каталог из git
+            echo "Восстановление каталога: $target_dir"
+            git restore --source=HEAD "$(basename "$target_dir")"
 
-        # Восстанавливаем каталог из git
-        echo "Восстановление каталога: $target_dir"
-        git restore --source=HEAD "$(basename "$target_dir")"
+            # Возвращаемся в исходный каталог
+            cd "$target_dir" || exit 1
+        fi
+    done
+}
 
-        # Возвращаемся в исходный каталог
-        cd "$current_dir" || exit 1
-    fi
-done
+process_directory "$(pwd)/../ts4-targets"
+process_directory "$(pwd)/../skt-sitrol/zz-releng/targets"
+
 
 echo "done"
 
